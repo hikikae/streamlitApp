cd2174fb3db7f81fcd0a464ab3b7d26ddaf03708
Angelica Garcia <angelicagarciad@gmail.com>
2023-09-19 17:13:39 -0600

    Update streamlit_app.py
    Picker por medio de frutas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,19 @@
+import pandas as pd
 import streamlit
 
+my_fruit_list= pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.title("Papaps")
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+#Seleccionador
+my_fruit_list=my_fruit_list.index('Fruit')
+streamlit.multiselect("Escoge cualquier fruta:", list(my_fruit_list.index))
+#Coloca la lista de donde se selecciona
+streamlit.dataframe(my_fruit_list)
