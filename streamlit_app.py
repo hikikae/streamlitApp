eaf5b6e9b75b0124166ed2a996d20e56b56dfb3f
Angelica Garcia <angelicagarciad@gmail.com>
2023-09-19 17:39:36 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ streamlit.dataframe(my_fruit_list)
 
 ### Para poder seleccionar de verdad avocado and banana
 my_fruit_lista= pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_lista = my_fruit_list.set_index('Fruit')
+my_fruit_lista = my_fruit_lista.set_index('Fruit')
 
 fruits_selected = streamlit.multiselect("Escoge cualquier fruta:", list(my_fruit_lista.index),['Avocado','Banana'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
